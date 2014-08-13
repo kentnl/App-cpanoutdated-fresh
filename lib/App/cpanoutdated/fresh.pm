89fefce07e0aa4611a6b2f53761ef098ed21260e
Kent Fredric <kentfredric@gmail.com>
2014-08-13 13:50:54 +1200

    v0.001000
     - First version.

--- lib/App/cpanoutdated/fresh.pm
+++ lib/App/cpanoutdated/fresh.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package App::cpanoutdated::fresh;
 
-our $VERSION = '0.001000';
+our $VERSION = '0.001001';
 
 # ABSTRACT: Indicate out-of-date modules by walking the metacpan releases backwards
 
