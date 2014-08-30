c4274233a59c316861e1c1cae4e0659c010891e1
Kent Fredric <kentfredric@gmail.com>
2014-08-30 12:41:29 +1200

    Build results of b7f603b (on master)

--- lib/App/cpanoutdated/fresh.pm
+++ lib/App/cpanoutdated/fresh.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package App::cpanoutdated::fresh;
 
-our $VERSION = '0.001001';
+our $VERSION = '0.001002';
 
 # ABSTRACT: Indicate out-of-date modules by walking the metacpan releases backwards
 
@@ -280,7 +280,7 @@ App::cpanoutdated::fresh - Indicate out-of-date modules by walking the metacpan 
 
 =head1 VERSION
 
-version 0.001001
+version 0.001002
 
 =head1 METHODS
 
