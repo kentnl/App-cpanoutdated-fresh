b7f603b36f67638b2dfa85c26c84e0ece20b938b
Kent Fredric <kentfredric@gmail.com>
2014-08-30 12:41:22 +1200

    v0.001001
     [00 Trivial]
     - metadata
    
     [Dependencies::Stats]
     - Dependencies changed since 0.001000, see misc/*.deps* for details
     - develop: +1 ↑2 (suggests: ↑2)
    
     [Metadata]
     - Fix author

--- lib/App/cpanoutdated/fresh.pm
+++ lib/App/cpanoutdated/fresh.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package App::cpanoutdated::fresh;
 
-our $VERSION = '0.001001';
+our $VERSION = '0.001002';
 
 # ABSTRACT: Indicate out-of-date modules by walking the metacpan releases backwards
 
